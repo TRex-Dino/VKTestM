d73f706748fc6d86afe628b11159a3298a5e3090
Dima <dmitrysinkov6@gmail.com>
2021-08-14 21:36:07 +0300

    delete default VC and main storyboard add AuthVC and AuthStoryboard
    add authService class
    add button and label to storyboard

---
+++ VKTestM/AuthViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AuthViewController.swift
+//  VKTestM
+//
+//  Created by Dmitry on 14.08.2021.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func signInVK(_ sender: UIButton) {
+        
+    }
+}
+
---
+++ VKTestM/Services/AuthSerivce.swift
@@ -0,0 +1,43 @@
+//
+//  AuthSerivce.swift
+//  VKTestM
+//
+//  Created by Dmitry on 14.08.2021.
+//
+
+import Foundation
+import VK_ios_sdk
+
+class AuthSerivce: NSObject, VKSdkDelegate, VKSdkUIDelegate {
+   
+    private let appId = "7926084"
+    private let vkSdk: VKSdk
+    
+    override init() {
+        vkSdk = VKSdk.initialize(withAppId: appId)
+        super.init()
+        print("VKSdk.initialize")
+        vkSdk.register(self)
+        vkSdk.uiDelegate = self
+    }
+    
+    func wakeUpSession() {
+        
+    }
+    
+    func vkSdkAccessAuthorizationFinished(with result: VKAuthorizationResult!) {
+        print(#function)
+    }
+    
+    func vkSdkUserAuthorizationFailed() {
+        print(#function)
+    }
+    
+    func vkSdkShouldPresent(_ controller: UIViewController!) {
+        print(#function)
+    }
+    
+    func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
+        print(#function)
+    }
+}
