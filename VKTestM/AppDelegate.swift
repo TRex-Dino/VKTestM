9a39e795d9d93a8ee496ca35fc41df24b79364c0
Dima <dmitrysinkov6@gmail.com>
2021-08-14 20:39:15 +0300

    start project add freamwork vksdk

--- VKTestM/AppDelegate.swift
+++ VKTestM/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import VK_ios_sdk
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        VKSdk.processOpen(url, fromApplication: UIApplication.OpenURLOptionsKey.sourceApplication.rawValue)
+        return true
+    }
+    
 
     // MARK: UISceneSession Lifecycle
 
