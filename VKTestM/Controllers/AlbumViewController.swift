52251aaee355d461eff8b45f759321b57d3b5954
Dima <dmitrysinkov6@gmail.com>
2021-08-16 12:36:21 +0300

    check image from url use image to cell

--- VKTestM/Controllers/AlbumViewController.swift
+++ VKTestM/Controllers/AlbumViewController.swift
@@ -12,19 +12,21 @@ class AlbumViewController: UICollectionViewController {
     private let reuseIdentifier = "cell"
     private var dataFetcher = NetworkDataFetcher(networking: NetworkService())
     
+    private let itemsPerRow: CGFloat = 2
+    private let sectionInsets = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+
+        
+//        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+//        layout.itemSize = CGSize(width: 70, height: 30)
+//        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+//        layout.minimumLineSpacing = 1
+//        layout.minimumInteritemSpacing = 1
+//        layout.scrollDirection = .vertical
+//
+//        collectionView.showsVerticalScrollIndicator = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-        
-        dataFetcher.getPhotos { photos in
-            guard let album = photos else { return }
-            album.items.map { photosItem in
-                print(photosItem)
-                print(photosItem.id)
-                
-            }
-        }
     }
 }
 
@@ -35,13 +37,52 @@ extension AlbumViewController {
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 10
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PhotoCell
+        
+        dataFetcher.getPhotos { photosResponse in
+            guard let photosResponse = photosResponse else { return }
+            photosResponse.sizes.map { photosUrl in
+                
+                guard let url = URL(string: photosUrl.url) else { return }
+                URLSession.shared.dataTask(with: url) { data, response, error in
+                    if let data = data, let image = UIImage(data: data) {
+                        DispatchQueue.main.async {
+                            cell.photoCell.sizeToFit()
+                            cell.photoCell.image = image
+                        }
+                    }
+                }.resume()
+            }
+        }
+        
         return cell
+    }
+}
+
+extension AlbumViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let paddingWidth = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = collectionView.frame.width - paddingWidth
+        let widthPerItem = availableWidth / itemsPerRow
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        sectionInsets
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        sectionInsets.left
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        sectionInsets.left
     }
 }
 
